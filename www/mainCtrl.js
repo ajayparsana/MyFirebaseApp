b2b88152561596c04eb5b84308ca48a266f6b003
unknown <ajayp@ajayp-txl.mumbai1.corp.citiustech.com>
2015-10-01 23:56:18 +0530

    Final changes

--- www/mainCtrl.js
+++ www/mainCtrl.js
@@ -1,21 +1,19 @@
 angular.module('starter').controller('mainCtrl', function ($scope,$firebaseAuth,$ionicLoading ) {
 
-    
+    $scope.data={};
+    $scope.data.text = 'Click Authenticate';
     var authObj = $firebaseAuth(new Firebase('https://ajayparsana.firebaseio.com'));
     $scope.authenticateUser = function () {
-        $ionicLoading.show({
-        template: 'Validating...'
-    });
+       $scope.data.text = 'Validating..';
         authObj.$authWithPassword({
             email: 'test@test.com',
             password: 'Test@123'
         }).then(function (authData) {
-            $ionicLoading.hide();
-            alert('Login Successful');
+            $scope.data.text = 'Success ' + authData.uid;
+            
         }).catch(function (error) {
-            $ionicLoading.hide();
-            alert( "Authentication failed: " + error);
-        });
+            $scope.data.text = "Authentication failed: " + error
+            });
 
     }
 
